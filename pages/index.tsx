61ff9fef2743637fdbded79a789b84f42ed10fe5
Udayan Shevade <udayan.shevade@gmail.com>
2020-11-05 23:52:30 -0800

    init, add typescript

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Simple Chat Client</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>Hello</main>
+    </div>
+  );
+}
